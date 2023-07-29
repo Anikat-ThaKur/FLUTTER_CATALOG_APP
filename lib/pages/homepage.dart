780893549e79a831f0e994081a9258c5ca890272
Anikat-ThaKur <anikatthakur1605@gmail.com>
2023-07-29 12:15:59 +0530

    Day8 completed

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,33 +1,105 @@
+import "dart:convert";
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 import "package:flutter_projects/models/catalog.dart";
 import "package:flutter_projects/widgets/Drawer.dart";
 import "package:flutter_projects/widgets/item_widget.dart";
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
 
-  int day =1;
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogjson=await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData= jsonDecode(catalogjson);
+    var productsData= decodedData["products"];
+    CatalogModel.items = List.from(productsData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {
+      
+    });
+
+
+    
+
+  }
 
   @override
   Widget build(BuildContext context) {
      
-     final dummyList =List.generate(10, (index)=> CatalogModel.items[0]);
+     
      return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           "Catalog App"
           )
         
       ),
         body: Padding(
           padding: const EdgeInsets.all(10.0),
-          child: ListView.builder(
-            itemCount: dummyList.length,
-            //for displaying the item
-            itemBuilder: (context,index) {
-              return ItemWidget(
-                item: dummyList[index]);
-        
+          child: (CatalogModel.items.isNotEmpty && CatalogModel.items.length > 1 )
+           ? GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 16,
+              mainAxisSpacing: 16,
+
+              ), 
+            itemBuilder: (context,index){
+              final item = CatalogModel.items[index];
+              return Card(
+                clipBehavior: Clip.antiAlias,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: GridTile(
+                  header: Container(
+                    padding: const EdgeInsets.all(12),
+                    decoration: const BoxDecoration(
+                      color: Colors.deepPurple,
+
+                    ),
+                    child: Text(
+                      item.name,
+                      style: const TextStyle(color: Colors.white),
+                      ),
+                    
+
+                    ),
+                  child: Image.network(item.image),
+                  footer: Container(
+                    padding: const EdgeInsets.all(12),
+                    decoration: const BoxDecoration(
+                      color: Colors.black,
+                  ),
+                    child: Text(
+                      item.price.toString(),
+                      style: const TextStyle(color: Colors.white),
+                      )
+                  
+                  )
+              
+
+                
+                )
+              );
             },
+            itemCount: CatalogModel.items.length,
+
+
+            )
+           : Center(
+            child: CircularProgressIndicator(),
           ),
         ),
       drawer: MyDrawer(),
